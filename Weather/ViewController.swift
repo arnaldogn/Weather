49873d26b3eafd4df5f54a6f63f562824d937f2b
Arnaldo Gnesutta <arnaldogn@gmail.com>
2022-05-13 16:08:17 +0200

    Initial commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Allen Gilliam on 13/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Weather/WeatherViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherViewModel.swift
+//  Weather
+//
+//  Created by Allen Gilliam on 13/5/22.
+//
+
+import Foundation
+/Users/allengilliam/Documents/Weather
